ccc58d96758f7bde3f9497301c16aefc1667380e
AR1 <s0802112@stud.uni-frankfurt.de>
2024-08-15 19:19:46 +0200

    Initial commit

---
+++ src/main/java/net/a/my/mod/MyModClient.java
@@ -0,0 +1,14 @@
+package net.a.my.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyModClient implements ClientModInitializer {
+
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
